6cfdcd3d40bfc017c11fd635b54faed7704529ec
Aadam Ali <57071686+Aadam-Ali@users.noreply.github.com>
2023-03-02 15:47:59 +0000

    BAU Address platop-pr-bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,6 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapPlayVersion,
     "uk.gov.hmrc" %% "play-frontend-hmrc"         % "6.7.0-play-28",
-    "uk.gov.hmrc" %% "play-language"              % "6.1.0-play-28"
   )
 
   val test = Seq(
