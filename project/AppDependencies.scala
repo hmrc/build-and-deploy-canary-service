3b953468999baec8cbd1ccce4e8c43269f5c0752
h-cave <124801533+h-cave@users.noreply.github.com>
2023-12-06 13:46:45 +0000

    BAU: Update play-hmrc-frontend to latest

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "7.23.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "7.29.0-play-28",
   )
 
   val test = Seq(
