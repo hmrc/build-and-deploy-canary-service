765d41dd16bcd0a550115e95fe267ab89dec4be7
darren.wood <205897253+DarrenWoodHMRC@users.noreply.github.com>
2025-06-18 11:17:48 +0100

    BAU update deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,11 +5,11 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapPlayVersion = "9.11.0"
+  val bootstrapPlayVersion = "9.13.0"
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.0.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.6.0"
   )
 
   val test = Seq(
