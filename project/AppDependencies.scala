aa538a9382120c588131b6abd7bc1fede0fe5360
Chindi-M <179483549+Chindi-M@users.noreply.github.com>
2026-02-25 12:21:49 +0000

    BAU-25022026 update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,11 +5,11 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapPlayVersion = "10.4.0"
+  val bootstrapPlayVersion = "10.6.0"
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.20.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.31.0"
   )
 
   val test = Seq(
