92ce943148e9abed82fc0aeb4cb85c92e355a44c
PTMGitHub <61422343+PTMGitHub@users.noreply.github.com>
2026-08-13 07:31:03 +0100

    PBD-BAU Update Canary service dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "13.9.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "13.11.0"
   )
 
   val test = Seq(
